77f04c60ba982fe5762b358ef574a6448d2ce58d
Victor Yunusa <victoryunusa17@gmail.com>
2018-09-06 14:15:28 +0000

    Load tasks from Tasks collection

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,11 @@
 import { Template } from 'meteor/templating';
  
+import { Tasks } from '../api/tasks.js';
+ 
 import './body.html';
  
 Template.body.helpers({
-  tasks: [
-    { text: 'This is Victor Yunusa' },
-    { text: 'I love You!' },
-    { text: 'This is task 3' },
-    { text: 'This is task 2' },
-    { text: 'This is task 3' },
-  ],
+  tasks() {
+    return Tasks.find({});
+  },
 });
