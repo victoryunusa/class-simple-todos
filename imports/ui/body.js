d20e6c278ec1ec45fb9b2bcfce4e2c01a0847ace
Victor Yunusa <victoryunusa17@gmail.com>
2018-09-06 14:13:16 +0000

    Load tasks collection on the server

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -4,7 +4,9 @@ import './body.html';
  
 Template.body.helpers({
   tasks: [
-    { text: 'This is task 1' },
+    { text: 'This is Victor Yunusa' },
+    { text: 'I love You!' },
+    { text: 'This is task 3' },
     { text: 'This is task 2' },
     { text: 'This is task 3' },
   ],
